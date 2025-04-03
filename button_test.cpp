2b99bfe457adca573f3d8602d87d52b05ea3c97f
Aiden Robinson <aiden.robinson1@gmail.com>
2025-04-03 15:55:24 -0400

    all 5 buttons working on button test

--- button_test.cpp
+++ button_test.cpp
@@ -1,11 +1,14 @@
 #include "DebouncedInterrupt.h"
 #include "mbed.h"
 
+
 // Define the LED and button
 DigitalOut led(PE_8); // LED connected to PE_8
 DebouncedInterrupt button1(PA_3);
 DebouncedInterrupt button2(PC_1);
 DebouncedInterrupt button3(PC_0);
+DebouncedInterrupt button4(PF_9);
+DebouncedInterrupt button5(PF_7);
 
 // Function to toggle the LED state
 void toggle_led() { led = !led; }
@@ -15,6 +18,8 @@ int main() {
   button1.attach(&toggle_led, IRQ_FALL, 20, false); // 20ms debounce time
   button2.attach(&toggle_led, IRQ_FALL, 20, false); // 20ms debounce time
   button3.attach(&toggle_led, IRQ_FALL, 20, false); // 20ms debounce time
+  button4.attach(&toggle_led, IRQ_FALL, 20, false); // 20ms debounce
+  button5.attach(&toggle_led, IRQ_FALL, 20, false); // 20ms debounce time
 
   // Enable interrupts
   __enable_irq();
@@ -31,4 +36,6 @@ PA_3
 PC_1
 PC_0
 
+PF_9
+PF_7
 */
